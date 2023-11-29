95a8d5a3c08a93954b2a4d10928b129bf53ac189
PAFF <87707468+toooki@users.noreply.github.com>
2023-11-30 00:56:49 +0900

    23-11-30
    23-11-30

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+require('dotenv').config()
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const passport = require('passport');
 require('./app_api/models/db');
+require('./app_api/config/passport');
 
 var indexRouter = require('./app_server/routes/index');
 const apiRouter = require('./app_api/routes/index');
@@ -11,7 +14,6 @@ const apiRouter = require('./app_api/routes/index');
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'pug');
 
@@ -20,11 +22,37 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app_public', 'build')));
+app.use(passport.initialize());
 
+app.use('/api', (req, res, next) => {
+  // res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Origin', '*');
+  req.header('Access-Control-Allow-Headers', 'Origin, X-Requested-with,  Content-Type, Accept');
+  next();
+});
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
-// app.use('/users', usersRouter);
 
+app.use('/api', (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-with, \
+  Content-type, Accept, Authorization");
+  next();
+});
+
+
+app.get(/(\about)|(\/location\/[a-z0-9]{24})/, function(req,res,next) {
+  res,sendFile(path.join(__dirnamem, 'app_public', 'build', 'index.html'));
+});
+
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError'){
+    res
+      .status(401)
+      .json({"message": err.name + ": " + err.message});
+  }
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -40,5 +68,10 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+//cors 오류 해결
+const cors = require('cors');
+
+app.use(cors());
 
 module.exports = app;
